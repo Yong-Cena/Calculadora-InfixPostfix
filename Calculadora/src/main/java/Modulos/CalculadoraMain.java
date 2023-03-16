4ed3c17c7c231f6b04917ebc0553c23db311f4f7
AXEL CASTRO LARA <acastr48@itam.mx>
2023-03-16 16:05:18 -0600

    inicio

---
+++ Calculadora/src/main/java/Modulos/CalculadoraMain.java
@@ -0,0 +1,12 @@
+package Modulos;
+
+/**
+ *
+ * @author ACASTROLA
+ */
+public class CalculadoraMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
